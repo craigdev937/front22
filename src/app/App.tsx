628c481aa309b40ef3365a62539c7c6fbdb11caa
Craig <indigomx9@gmail.com>
2023-03-31 20:26:59 -0400

    Initial setup of the project.

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, reiciendis corporis! Officia, ex tempore praesentium commodi ratione sapiente sint rerum quidem velit eligendi molestias dolorem, error pariatur sed, quis reprehenderit.</p>
+        </React.Fragment>
+    );
+};
+
+
